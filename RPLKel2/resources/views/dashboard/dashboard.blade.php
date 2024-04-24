b7b0e5c3778725106618c0b4cb517ff1ec3a3c64
zaimxyz <mzaima22@gmail.com>
2024-04-24 13:06:14 +0700

    sprint 1 tambahan

--- RPLKel2/resources/views/dashboard/dashboard.blade.php
+++ RPLKel2/resources/views/dashboard/dashboard.blade.php
@@ -235,88 +235,95 @@
                 Course Overview
         </div>
 
-        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <div class="container">
-                        <div class="row">
-
-                            <div class="col-md-3">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <h5 class="card-title">Card 1</h5>
-                                        <p class="card-text">Content of card 1.</p>
+        <div id="card-body">
+            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <div class="card">
+                                    <img src=".jpg" class="card-img-top" alt="Image for Card 1">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Mathematics</h5>
+                                            <a href=" " class="btn btn-primary">Go to Course</a>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
 
-                            <div class="col-md-3">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <h5 class="card-title">Card 2</h5>
-                                        <p class="card-text">Content of card 2.</p>
+                                <div class="col-md-3">
+                                    <div class="card">
+                                    <img src=".jpg" class="card-img-top" alt="Image for Card 2">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Physics</h5>
+                                            <a href=" " class="btn btn-primary">Go to Course</a>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
 
-                            <div class="col-md-3">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <h5 class="card-title">Card 3</h5>
-                                        <p class="card-text">Content of card 3.</p>
+                                <div class="col-md-3">
+                                    <div class="card">
+                                    <img src=".jpg" class="card-img-top" alt="Image for Card 3">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Biology</h5>
+                                            <a href=" " class="btn btn-primary">Go to Course</a>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
 
-                            <div class="col-md-3">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <h5 class="card-title">Card 4</h5>
-                                        <p class="card-text">Content of card 4.</p>
+                                <div class="col-md-3">
+                                    <div class="card">
+                                    <img src=".jpg" class="card-img-top" alt="Image for Card 4">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Chemistry</h5>
+                                            <a href=" " class="btn btn-primary">Go to Course</a>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
 
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="carousel-item">
+                        <div class="container">
+                            <div class="row">
+
+                            <div class="col-md-3">
+                                    <div class="card">
+                                    <img src=".jpg" class="card-img-top" alt="Image for Card 5">
+                                        <div class="card-body">
+                                            <h5 class="card-title">English</h5>
+                                            <a href=" " class="btn btn-primary">Go to Course</a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-3">
+                                    <div class="card">
+                                    <img src=".jpg" class="card-img-top" alt="Image for Card 6">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Lorem Ipsum</h5>
+                                            <a href=" " class="btn btn-primary">Go to Course</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            
+                            </div>
                         </div>
                     </div>
                 </div>
 
-                <div class="carousel-item">
-                    <div class="container">
-                        <div class="row">
-
-                            <div class="col-md-3">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <h5 class="card-title">Card 5</h5>
-                                        <p class="card-text">Content of card 5.</p>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-md-3">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <h5 class="card-title">Card 6</h5>
-                                        <p class="card-text">Content of card 6.</p>
-                                    </div>
-                                </div>
-                            </div>
-                        
-                        </div>
-                    </div>
-                </div>
+                <button class="carousel-control-prev bg-black" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev" style="width: 25px;height: 25px; border-radius:100%; margin-top:60px; left: 3px">
+                    <span class="carousel-control-prev-icon" aria-hidden="true" style="margin-left:0px"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                
+                <button class="carousel-control-next bg-black" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next" style="width: 25px;height: 25px; border-radius:100%; margin-top:65px; right: 3px">
+                    <span class="carousel-control-next-icon" aria-hidden="true" style="margin-left:0px"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
             </div>
-
-            <button class="carousel-control-prev bg-black" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev" style="width: 25px;height: 25px; border-radius:100%; margin-top:50px; left: -15px">
-                <span class="carousel-control-prev-icon" aria-hidden="true" style="margin-left:0px"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            
-            <button class="carousel-control-next bg-black" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next" style="width: 25px;height: 25px; border-radius:100%; margin-top:50px ;right: -15px">
-                <span class="carousel-control-next-icon" aria-hidden="true" style="margin-left:0px"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
-        </div>     
+        </div>
     </div>
 @endsection
